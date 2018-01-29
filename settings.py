b8e2b6821a70112f8c286922b9c920e41593181b
Andrei Fyodorov <andrey.fedorov@booking.com>
2018-01-29 02:16:00 +0100

    Add token (secretly)

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-TOKEN = ''
+from secret import TOKEN
+
 CERT = '/usr/local/etc/nginx/cert.pem'
-WEBHOOK_HOST = 'webhooks.bakunin.nl'
+WEBHOOK_HOST = 'webhooks.bakunin.nl/mud'
 REDIS = {'host': 'localhost', 'port': 6379}
