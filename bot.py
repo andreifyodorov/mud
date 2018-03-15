c056681f7fb2076755acfa7540c84a4234497a29
Andrei Fyodorov <andrey.fedorov@booking.com>
2018-03-15 01:17:27 +0100

    Combine all message to one chatkey

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
+
 from mud.chatflow import CommandPrefix
+import settings
 
 import telegram
-import settings
 
 
 class Bot(telegram.Bot):
@@ -9,17 +11,18 @@ class Bot(telegram.Bot):
 
     def __init__(self, *args, **kwargs):
         super(Bot, self).__init__(settings.TOKEN, *args, **kwargs)
-        self.message_queue = []
+        self.message_queue = defaultdict(list)
 
     def send_callback_factory(self, chatkey):
         def callback(msg):
-            self.message_queue.append(dict(chat_id=chatkey, text=msg))
+            self.message_queue[chatkey].append(msg)
         return callback
 
     def send_messages(self):
         while self.message_queue:
-            message = self.message_queue.pop(0)
-            self.sendMessage(parse_mode="Markdown", **message)
+            chat_id, messages = self.message_queue.popitem()
+            text = "\n".join(messages)
+            self.sendMessage(parse_mode="Markdown", chat_id=chat_id, text=text)
 
 
 bot = Bot()
